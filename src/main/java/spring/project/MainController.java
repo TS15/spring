c68465cd2a764901d65c36fec8943edb40b0ae82
TS15 <savko_t@mail.ru>
2018-09-23 23:55:55 +0200

    Front

--- src/main/java/spring/project/MainController.java
+++ src/main/java/spring/project/MainController.java
@@ -3,17 +3,48 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import spring.project.domain.actor;
 import spring.project.mapper.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+
+@Controller
 public class MainController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
     private final actorMapper actormapper;
-   // private final addresMapper addressmapper;
+    
+    @GetMapping("/")
+    public String home(ModelMap modelMap) {
+    	return "index";
+
+    }
+    @GetMapping("/contact.html")
+    public String contact(ModelMap modelMap) {
+    	return "contact";
+
+    }
+    @GetMapping("/contact_eng.html")
+    public String contacteng(ModelMap modelMap) {
+    	return "contact_eng";
+
+    }
+    @GetMapping("/index.html")
+    public String index(ModelMap modelMap) {
+    	return "index";
+
+    }
+    @GetMapping("/index_eng.html")
+    public String indexeng(ModelMap modelMap) {
+    	return "index_eng";
+
+    }
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
@@ -27,12 +58,7 @@ public class MainController {
     public MainController(actorMapper actormapper) {
     	this.actormapper = actormapper;
     }
- /*   @RequestMapping("/address")
-    public address address(@RequestParam(value="id", defaultValue = "1") String id) {
-    	return this.addresmapper.findById(id);
-    }
-    public MainController(addresMapper addressmapper) {
-    	this.addresmapper = addressmapper;
-    }
-    */
+
+    
+    
 }	
